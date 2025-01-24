aedad5b3ef763fb98b0e670c6aff64f72161c103
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2025-01-24 16:02:24 -0300

    Creating the server startup

---
+++ src/main/java/nom/brunokarpo/server/GreetingServer.java
@@ -0,0 +1,30 @@
+package nom.brunokarpo.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50051;
+
+        // Create a new server to listen on port
+        Server server = ServerBuilder.forPort(port).build();
+
+        // Starting the server
+        server.start();
+
+        System.out.println("Server started, listening on " + port);
+
+        // Adding shutdown hook gracefully
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down gRPC server since JVM is shutting down");
+            server.shutdown();
+            System.out.println("Server shut down");
+        }));
+
+        server.awaitTermination();
+    }
+}
